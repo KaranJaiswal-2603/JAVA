ce0de7ae8acffd66b2e095839f9d143725c1251d
Karan Jaiswal <jaiswalkaran6289@gmail.com>
2026-08-21 12:15:34 +0530

    Delete negative.java

--- negative.java
+++
@@ -1,8 +0,0 @@
-public class negative{
-    public static void main(String[] args) {
-        int num = -12;
-        if (num < 0){
-            System.out.println("Negaive Number");
-        }
-    }
-}
