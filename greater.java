da39a24c25abd281d0dcfecc7be977df49f5eafb
Karan Jaiswal <jaiswalkaran6289@gmail.com>
2026-08-21 12:18:15 +0530

    Delete greater.java

--- greater.java
+++
@@ -1,8 +0,0 @@
-public class greater{
-    public static void main(String[] args) {
-        int num = 101;
-        if(num>100){
-            System.out.println("Number is greater then 100");
-        }
-    }
-}
